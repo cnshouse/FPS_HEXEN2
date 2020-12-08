52830472d0d1e8c4c7bb0a93769c7bdc770c152c
cnshouse <cnshouse@gmail.com>
2020-12-08 12:37:20 -0800

    Knights
    First step in introducing medival character. Knight 3rd person and Knight bots wielding machine guns....

---
+++ FPS_Hexen2/Assets/Addons/Customizer/Content/Example/Model/SMG/ScaleLocker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleLocker : MonoBehaviour
+{
+    public Vector3 _scale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _scale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.localScale != _scale)
+		{
+            transform.localScale = _scale;
+		}
+    }
+}
