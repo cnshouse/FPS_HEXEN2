055fda2b0847b6cbb6bc10597dc8c8fb3f6783bb
cnshouse <cnshouse@gmail.com>
2021-02-17 14:05:35 -0800

    Cover Mode Add

---
+++ FPS_Hexen2/Assets/Addons/CovertPoint/Content/Scripts/Editor/CoverPointInitializer.cs
@@ -0,0 +1,114 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine.SceneManagement;
+using MFPSEditor;
+
+public class CoverPointInitializer : MonoBehaviour
+{
+    private const string DEFINE_KEY = "CP";
+
+    static CoverPointInitializer()
+    {
+        int start = PlayerPrefs.GetInt("mfps.addons.define." + DEFINE_KEY, 0);
+        if (start == 0)
+        {
+            bool defines = EditorUtils.CompilerIsDefine(DEFINE_KEY);
+            if (!defines)
+            {
+                EditorUtils.SetEnabled(DEFINE_KEY, true);
+            }
+            PlayerPrefs.SetInt("mfps.addons.define." + DEFINE_KEY, 1);
+        }
+    }
+
+    [MenuItem("MFPS/Addons/CovertPoint/Enable")]
+    private static void Enable()
+    {
+        EditorUtils.SetEnabled(DEFINE_KEY, true);
+    }
+
+    [MenuItem("MFPS/Addons/CovertPoint/Enable", true)]
+    private static bool EnableValidate()
+    {
+        return !EditorUtils.CompilerIsDefine(DEFINE_KEY);
+    }
+
+    [MenuItem("MFPS/Addons/CovertPoint/Disable")]
+    private static void Disable()
+    {
+        if (bl_GameData.Instance.gameModes.Exists(x => x.gameMode == GameMode.CP))
+        {
+            int id = bl_GameData.Instance.gameModes.FindIndex(x => x.gameMode == GameMode.CP);
+            bl_GameData.Instance.gameModes[id].isEnabled = false;
+            EditorUtility.SetDirty(bl_GameData.Instance);
+            AssetDatabase.SaveAssets();
+        }
+        EditorUtils.SetEnabled(DEFINE_KEY, false);
+    }
+
+    [MenuItem("MFPS/Addons/CovertPoint/Disable", true)]
+    private static bool DisableValidate()
+    {
+        return EditorUtils.CompilerIsDefine(DEFINE_KEY);
+    }
+
+    [MenuItem("MFPS/Addons/CovertPoint/Integrate")]
+    private static void Instegrate()
+    {
+        bl_RoomSettings lb = FindObjectOfType<bl_RoomSettings>();
+        if (lb != null)
+        {
+            GameObject prefab = AssetDatabase.LoadAssetAtPath("Assets/Addons/CovertPoint/Content/Prefabs/CovertPoint.prefab", typeof(GameObject)) as GameObject;
+            if (prefab != null)
+            {
+                GameObject g = PrefabUtility.InstantiatePrefab(prefab, EditorSceneManager.GetActiveScene()) as GameObject;
+                GameObject gmo = GameObject.Find("GameModes");
+                if (gmo != null)
+                {
+                    g.transform.parent = gmo.transform;
+                    g.transform.localPosition = Vector3.zero;
+                }
+                g.transform.SetAsLastSibling();
+                if (!bl_GameData.Instance.gameModes.Exists(x => x.gameMode == GameMode.CP))
+                {
+                    GameModeSettings gmi = new GameModeSettings();
+                    gmi.gameMode = GameMode.CP;
+                    gmi.ModeName = "Covert Point";
+                    gmi.isEnabled = true;
+                    gmi.RequiredPlayersToStart = 2;
+                    bl_GameData.Instance.gameModes.Add(gmi);
+                    EditorUtility.SetDirty(bl_GameData.Instance);
+                    AssetDatabase.SaveAssets();
+                }
+                else
+                {
+                    int id = bl_GameData.Instance.gameModes.FindIndex(x => x.gameMode == GameMode.CP);
+                    bl_GameData.Instance.gameModes[id].isEnabled = true;
+                    EditorUtility.SetDirty(bl_GameData.Instance);
+                    AssetDatabase.SaveAssets();
+                }
+                EditorUtility.SetDirty(g);
+                EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+
+                EditorGUIUtility.PingObject(g);
+                var view = (SceneView)SceneView.sceneViews[0];
+                view.ShowNotification(new GUIContent("Cover Point integrate in this map!"));
+                Debug.Log("<color=green><b>Cover Point</b> integrate in this map!</color>");
+            }
+            else
+            {
+                Debug.Log("Can't found prefab!");
+            }
+        }
+    }
+
+    [MenuItem("MFPS/Addons/CovertPoint/Integrate", true)]
+    private static bool InstegrateValidate()
+    {
+        bl_CoverPoint km = GameObject.FindObjectOfType<bl_CoverPoint>();
+        bl_GameManager gm = GameObject.FindObjectOfType<bl_GameManager>();
+        return (km == null && gm != null);
+    }
+}
---
+++ FPS_Hexen2/Assets/Addons/CovertPoint/Content/Scripts/bl_CoverPointBase.cs
@@ -0,0 +1,190 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using Photon.Pun;
+using Photon.Realtime;
+
+namespace MFPS.GameModes.CoverPoint
+{
+    public class bl_CoverPointBase : bl_PhotonHelper
+    {
+        public int PointID = 0;
+
+        [Header("HUD Icon")]
+        public Texture2D Icon;
+        public Vector3 Offset;
+        public float size = 25;
+        public Color iconColor = Color.white;
+
+        [Header("References")]
+        public MeshRenderer BaseRenderer;
+        public Image BaseIcon;
+        public Image BackgroundImg;
+
+        //Private
+        private int cacheDomineIn = 0;
+        public bool isLocalIn { get; set; }
+        private bl_CPBaseInfo info
+        {
+            get { return bl_CoverPoint.Instance.objetivePoints[PointID]; }
+        }
+        public float takingProgress { get; set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void Awake()
+        {
+            cacheDomineIn = bl_CoverPoint.Instance.TimeToDominatePoint;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void OnTriggerEnter(Collider c)
+        {
+            if (c.transform.CompareTag(bl_PlayerSettings.LocalTag))
+            {
+                isLocalIn = true;
+                bl_CoverPoint.Instance.OnLocalPlayerEnterInPoint(this);
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void OnTriggerExit(Collider c)
+        {
+            if (c.transform.CompareTag(bl_PlayerSettings.LocalTag))
+            {
+                bl_CoverPoint.Instance.OnLocalPlayerExitPoint(this);
+                isLocalIn = false;
+            }
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void StartTakingThisPoint(Team team)
+        {
+            StartCoroutine("FillIcon", team);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void StopTaking()
+        {
+            StopCoroutine("FillIcon");
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void CancelTaking()
+        {
+            StopCoroutine("FillIcon");
+            takingProgress = 0;
+            BaseIcon.fillAmount = 1;
+            Color c = info.TeamOwner.GetTeamColor(0.2f);
+            BackgroundImg.color = c;
+            BaseRenderer.material.SetColor("_TintColor", c);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        IEnumerator FillIcon(Team team)
+        {
+            Team playerTeam = team;
+            while (takingProgress < 1)
+            {
+                takingProgress += Time.deltaTime / cacheDomineIn;
+                BaseIcon.fillAmount = 1 - takingProgress;
+                Color c = Color.Lerp(info.TeamOwner.GetTeamColor(), playerTeam.GetTeamColor(), takingProgress);
+                c.a = 0.2f;
+                BaseRenderer.material.SetColor("_TintColor", c);
+                if (isLocalIn)
+                {
+                    bl_CovertPointUI.Instance.ShowBar(true, takingProgress, Mathf.FloorToInt(cacheDomineIn - (cacheDomineIn * takingProgress)));
+                }
+                yield return null;
+            }
+            OnDomine(team);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void OnDomine(Team team)
+        {
+            BaseIcon.fillAmount = 1;
+            bl_CovertPointUI.Instance.ShowBar(false);
+            info.TeamOwner = team;
+            info.pointStatus = CovertPointStatus.Taked;
+            takingProgress = 0;
+            BackgroundImg.color = team.GetTeamColor(0.2f);
+            if (PhotonNetwork.IsMasterClient)
+            {
+                string hexColor = ColorUtility.ToHtmlStringRGB(team.GetTeamColor());
+                bl_KillFeed.Instance.SendMessageEvent(string.Format("<color=#{0}>{1}</color> has taken {2} point", hexColor, team.GetTeamName(), info.Name));
+            }
+            //reward all players that capture this point
+            if (info.OwnerTeamContainPlayer(PhotonNetwork.LocalPlayer.ActorNumber))
+            {
+                PhotonNetwork.LocalPlayer.PostScore(bl_CoverPoint.Instance.PlayerScorePerCapture);
+            }
+            bl_CoverPoint.Instance.OnCoverPoint();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void RefreshInfo()
+        {
+            BaseIcon.fillAmount = 1;
+            Color c = info.TeamOwner.GetTeamColor(0.2f);
+            BackgroundImg.color = c;
+            BaseRenderer.material.SetColor("_TintColor", c);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        void OnGUI()
+        {
+            if (!bl_UtilityHelper.GetCursorState)
+                return;
+            if (bl_GameManager.Instance.CameraRendered == null)
+                return;
+            if (bl_RoomMenu.Instance.isPaused)
+                return;
+
+            Vector3 position = transform.position + Offset;
+            Plane plane = new Plane(bl_GameManager.Instance.CameraRendered.transform.forward, bl_GameManager.Instance.CameraRendered.transform.position);
+
+            //If the object is behind the camera, then don't draw it
+            if (plane.GetSide(position) == false)
+            {
+                return;
+            }
+
+            //Calculate the 2D position of the position where the icon should be drawn
+            Vector3 viewportPoint = bl_GameManager.Instance.CameraRendered.WorldToViewportPoint(position);
+
+            //The viewportPoint coordinates are between 0 and 1, so we have to convert them into screen space here
+            Vector2 drawPosition = new Vector2(viewportPoint.x * Screen.width, Screen.height * (1 - viewportPoint.y));
+
+            float clampBorder = 12;
+
+            //Clamp the position to the edge of the screen in case the icon would be drawn outside the screen
+            drawPosition.x = Mathf.Clamp(drawPosition.x, clampBorder, Screen.width - clampBorder);
+            drawPosition.y = Mathf.Clamp(drawPosition.y, clampBorder, Screen.height - clampBorder);
+
+            GUI.color = iconColor;
+            GUI.DrawTexture(new Rect(drawPosition.x - size * 0.5f, drawPosition.y - size * 0.5f, size, size), Icon);
+            GUI.color = Color.white;
+        }
+    }
+}
