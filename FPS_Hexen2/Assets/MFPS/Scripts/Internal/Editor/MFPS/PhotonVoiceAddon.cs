20e7572fd71b8fc3227a1908c5f91a1e20291a7d
cnshouse <cnshouse@gmail.com>
2021-02-24 15:00:28 -0800

    UPdates

--- FPS_Hexen2/Assets/MFPS/Scripts/Internal/Editor/MFPS/PhotonVoiceAddon.cs
+++ FPS_Hexen2/Assets/MFPS/Scripts/Internal/Editor/MFPS/PhotonVoiceAddon.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEditorInternal;
 using UnityEditor.SceneManagement;
 using MFPSEditor;
+using MFPS.Addon.PlayerSelector;
 #if !UNITY_WEBGL && PVOICE
 using Photon.Voice.Unity;
 using Photon.Voice.PUN;
@@ -40,12 +41,12 @@ public class PhotonVoiceAddon : MonoBehaviour
 
 #if PVOICE
         //setup the player 1
-        SetUpPlayerPrefab(bl_GameData.Instance.Player1.gameObject);
-        SetUpPlayerPrefab(bl_GameData.Instance.Player2.gameObject);
+        //SetUpPlayerPrefab(bl_GameData.Instance.Player1.gameObject);
+        //SetUpPlayerPrefab(bl_GameData.Instance.Player2.gameObject);
 
 
 #if PSELECTOR
-        foreach(var p in MFPS.PlayerSelector.bl_PlayerSelectorData.Instance.AllPlayers)
+        foreach(var p in bl_PlayerSelectorData.Instance.AllPlayers)
         {
             if (p.Prefab == null ) continue;
          SetUpPlayerPrefab(p.Prefab.gameObject);
